d50f8a0d803d0f9f9c415518f6d18189ba18fbe7
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-20 17:40:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,14 @@
-require "spec_helper"
+#require "spec_helper"
 
-describe "Artist" do 
-  it "has a name" do
-    artist = Artist.new
-    artist.name = "Beyonce" 
-    expect(artist.name).to eq("Beyonce")
-  end
-end
+#describe "Artist" do 
+  #it "has a name" do
+    #artist = Artist.new
+    #artist.name = "Beyonce" 
+    #expect(artist.name).to eq("Beyonce")
+  #nd
+#end
+
+class Artist
+  attr_accessor :name
+  
+  
